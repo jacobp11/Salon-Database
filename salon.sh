18bfbcc87651b74351850f83d51966979a61b96b
jacob p <jacobpanger@protonmail.com>
2024-07-09 19:45:26 +0000

    completed script

--- salon.sh
+++ salon.sh
@@ -19,8 +19,9 @@ MAIN_MENU() {
   echo -e "\nSelect a service to schedule.\n"
   read SERVICE_ID_SELECTED
 
-  # save service_id for later
+  # save service_id and service name for later
   SERVICE_ID=$($PSQL "SELECT service_id FROM services WHERE service_id = '$SERVICE_ID_SELECTED'")
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = '$SERVICE_ID_SELECTED'")
 
   # print selection to screen
   case $SERVICE_ID_SELECTED in
@@ -36,7 +37,6 @@ MAIN_MENU() {
 
   # check for phone number
   PHONE_NUMBER=$($PSQL "SELECT phone FROM customers WHERE phone = '$CUSTOMER_PHONE'")
-  echo $PHONE_NUMBER
 
   # if phone number does not exist
   if [[ -z $PHONE_NUMBER ]]
@@ -49,6 +49,21 @@ MAIN_MENU() {
     CUSTOMER_INSERT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
   fi
 
+  # request service time
+  echo -e "Please enter a time for your appointment.\n"
+  read SERVICE_TIME
+
+  # get customer_id
+  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+  
+  # insert appointment
+  APPOINTMENT_INSERT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES('$CUSTOMER_ID', '$SERVICE_ID', '$SERVICE_TIME')")
+
+  # get customer name
+  NAME=$($PSQL "SELECT name FROM customers WHERE customer_id = '$CUSTOMER_ID'")
+  
+  # end message
+  echo -e "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $NAME." | sed 's/  / /g'
 }
 
 MAIN_MENU
